de81770c4058871965401aafd44f69738341bf9d
barehacks <bareh4cks@gmail.com>
2021-09-05 21:41:55 +0800

    added home_screen.dart file

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:mobitek/screens/add_image_post.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        title: const Text(
+          "Home",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      // ignore: sized_box_for_whitespace
+      body: Container(
+        padding: const EdgeInsets.all(12),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(60.0),
+                  primary: Colors.grey,
+                  onPrimary: Colors.grey[100],
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => AddImagePost()));
+                },
+                child: const Text('Add Image')),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(60.0),
+                  primary: Colors.grey,
+                  onPrimary: Colors.grey[100],
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => AddImagePost()));
+                },
+                child: const Text('Image List')),
+          ],
+        ),
+      ),
+    );
+  }
+}
